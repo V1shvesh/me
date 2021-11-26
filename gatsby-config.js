7d2e1f311cf6d44c7dca5d10b5e9994926707dba
V1shvesh <s.vishvesh@grofers.com>
2021-11-27 02:10:31 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "crysna.dev",
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-react-helmet"],
+};
